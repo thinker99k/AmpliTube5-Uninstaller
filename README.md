d78d4936c8080a2f03767eeb1c12ed728ab9195d
thinker99k <thinker99k@gmail.com>
2026-07-25 06:07:37 +0900

    first release

--- README.md
+++ README.md
@@ -5,25 +5,22 @@
 1. Download *script.zsh*
 2. Open terminal
 3. Do
-
-    `xattr -d com.apple.quarantine FILE_LOCATION` : disable quarantine flag on script 
-    
-    `chmod +x FILE_LOCATION` : grant exec privilege to script
-
-   (To fill `FILE_LOCATION`, just drag *script.zsh* from finder to terminal)
+   1. **Unauthorize AmpliTube 5 in IK Product Manager**
+   2. `xattr -d com.apple.quarantine SCRIPT_PATH` : disable quarantine flag on script
+   3. `chmod +x SCRIPT_PATH` : grant execute privilege to script
+   4. `sudo SCRIPT_PATH` : actual run of script
+   
+      (To fill `FILE_LOCATION`, just drag *script.zsh* from finder to terminal)
 
 ## DISCLAIMER
 **USE AT YOUR OWN RISK**
 
-Author(s) of this repo do(es) not have charge on any malfunctions, so do backup important files before run this script
+Author(s) of this repo do(es) not have charge on any malfunctions, so backup important files before run this script
 
 Tested on
 - MacOS Seqouia 15.7.7
 - Mac Mini (2018)
 - AmpliTube 5 MAX v2, 5.10.9
-
-why the fucking git keeps making conflict??????
-
 
 ---
 Any PR will be appreciated!
--- script.zsh
+++ script.zsh
@@ -1,131 +1,100 @@
 #!/bin/zsh
 
-# for debug
-DEBUG=0
 
-function dbg(){
+### for debug
+DEBUG=1
+
+dbg(){
   if [[ $DEBUG -eq 1 ]]; then
-    out "DEBUG $1"
+    echo "DEBUG : $1"
   fi
 }
 
-function out_open(){
-  echo -n "$1"
+
+### global variables
+readonly PKG_NAME="com.ikmultimedia.AmpliTube5"
+readonly CORE_PATH="/Library/Application Support/IK Multimedia/AmpliTube 5"
+
+readonly PROC_NAME1="AmpliTube 5"
+readonly PROC_NAME2="IK Product Manager"
+
+readonly PKGFILES="pkgfiles.txt"
+readonly FILTERED="filtered.txt"
+
+
+### io
+readonly DEFAULT="\033[0m"
+readonly GREEN="\033[32m"
+readonly YELLOW="\033[33m"
+readonly RED="\033[31m"
+
+# color (color:string)
+color() {
+  case "$1" in
+    GREEN) echo "${GREEN}" ;;
+    YELLOW) echo "${YELLOW}" ;;
+    RED) echo "${RED}" ;;
+    *) echo "${DEFAULT}" ;;
+  esac
 }
 
-function out_close(){
-  echo "$1"
-}
+# msg_open (color:string) (msg:string)
+msg_open(){
+  local msg_color1
+  msg_color1=$(color "$1")
 
-function err_open(){
-  echo -n "$1" >&2
-}
+  # stderr
+  if [[ "$1" == "RED" ]]; then
+    printf "${msg_color1}%s${DEFAULT}" "$2" >&2
 
-function err_close(){
-  echo "$1" >&2
-}
-
-### 1
-PKG_NAME="com.ikmultimedia.AmpliTube5"
-PROC_NAME="AmpliTube 5"
-
-function chk_privilege() {
-  out_open "[1/7] Check root privilege... "
-
-  if [[ $EUID -ne 0 ]]; then
-    err_close "FAIL"
-    err_open "** ERROR ** "
-    err_close "Run script with root privilege"
-    exit 1
+  # stdout
   else
-    out_close "OK"
+    printf "${msg_color1}%s${DEFAULT}" "$2"
   fi
-
-  dbg "[1/7]"
 }
 
-function chk_package() {
-  out_open "[2/7] Check if package exists... "
+# msg_close (color:string) (msg:string)
+msg_close(){
+  local msg_color2
+  msg_color2=$(color "$1")
 
-  if ! pkgutil --pkgs | grep -q "^${PKG_NAME}$"; then
-    err_close "NO"
-    err_open "** Warning ** "
-    err_close "Nothing to uninstall"
-    exit 0
+  # stderr
+  if [[ "$1" == "RED" ]]; then
+    printf "${msg_color2}%s${DEFAULT}\n" "$2" >&2
+
+  # stdout
   else
-    out_close "OK"
+    printf "${msg_color2}%s${DEFAULT}\n" "$2"
   fi
-
-  dbg "[2/7]"
-}
-
-function chk_process() {
-  out_open "[3/7] Kill AmpliTube5 if running... "
-
-  if pgrep -x "$PROC_NAME" > /dev/null; then
-    out_open "FOUND... "
-
-    killall "$PROC_NAME" > /dev/null 2>&1
-    sleep 10
-  else
-    out_close "NOT RUNNING"
-    return
-  fi
-
-  # re-check is process killed
-  if pgrep -x "$PROC_NAME" > /dev/null; then
-    err_close "FAIL"
-    err_open "** ERROR ** "
-    err_close "Failed to terminate"
-    exit 1
-  else
-    out_close "OK"
-  fi
-
-  dbg "[3/7]"
 }
 
 
-### 2
-LIST_TXT="list.txt"
+# stat (color:string) (msg:string)
+stat(){
+  local stat_color
+  stat_color=$(color "$1")
 
-function do_make_list() {
+  local cols
+  cols=$(tput cols 2>/dev/null || echo 80)
+  local msg="[ $2 ]"
+  local col_pos=$(( cols - ${#msg} ))
 
-
-  dbg "[4/7]"
-}
-
-function do_format_list() {
-
-  dbg "[5/7]"
-}
-
-function do_remove_files() {
-
-  dbg "[6/7]"
+  tput hpa ${col_pos}
+  printf "[ ${stat_color}%s${DEFAULT} ]\n" "$2"
 }
 
 
-### 3
-function cleanup() {
-  rm -f "$LIST_TXT"
-  # pkgutil --forget "$PKG_NAME" > /dev/null 2>&1
-
-  # for debug
-  dbg "[7/7]"
-}
-
-function get_yn() {
+get_yn() {
   local answer
   local cnt=0
 
   while true; do
     if [[ $cnt -ge 3 ]]; then
-      echo "YOU MORON"
-      exit 0;
+    msg_close RED "YOU MORON"
+      exit 1;
     fi
 
-    out_open "$1"
+    msg_open DEFAULT "$1"
 
     read -r answer
     case "$answer" in
@@ -138,8 +107,8 @@ function get_yn() {
         ;;
 
       * )
-        err_open "** ERROR ** "
-        err_close "Invalid input."
+        msg_open YELLOW "** Warning ** "
+        msg_close YELLOW "Invalid input."
         ;;
     esac
 
@@ -147,28 +116,271 @@ function get_yn() {
   done
 }
 
-function main() {
-  out_close "[ AmpliTube5 Uninstaller ]"
+
+### 1
+chk_priv() {
+  msg_open DEFAULT "[1/7] Check root privilege... "
+
+  if [[ $EUID -ne 0 ]]; then
+    stat RED "FAIL"
+    msg_open RED "** ERROR ** "
+    msg_close RED "Run script with root privilege"
+    exit 1
+
+  else
+    stat GREEN "OK"
+  fi
+}
+
+chk_pkg() {
+  msg_open DEFAULT "[2/7] Check if ${PKG_NAME} exists... "
+
+  # installed
+  if pkgutil --pkgs | grep -q "^${PKG_NAME}$"; then
+    stat GREEN "OK"
+    return
+
+  # installed but NO receipt
+  # ex. user delete just AmpliTube 5.app from /Application
+  elif [[ -d "${CORE_PATH}" ]]; then
+    stat RED "FAIL"
+    msg_open RED "** ERROR ** "
+    msg_close RED "Package Receipt not found, reinstall Amplitube 5"
+    exit 1
+
+  # not installed
+  else
+    stat YELLOW "NO"
+    msg_open YELLOW "** Warning ** "
+    msg_close YELLOW "Nothing to uninstall"
+    exit 0
+  fi
+}
+
+kill_proc() {
+  # 1st try -> pkill (-15 : SIGTERM)
+  pkill -x "$1" > /dev/null 2>&1
+  sleep 2
+  if ! pgrep -x "$1" > /dev/null; then
+    return 0
+  fi
+
+  # 2nd try -> pkill force (-9 : SIGKILL)
+  pkill -9 -x "$1" > /dev/null 2>&1
+  sleep 2
+  if ! pgrep -x "$1" > /dev/null; then
+    return 0
+  fi
+
+  return 1
+}
+
+find_proc() {
+  # process found
+  if pgrep -x "$1" > /dev/null; then
+    msg_open DEFAULT "FOUND... "
+
+    # kill success
+    if kill_proc "$1"; then
+      return 0
+
+    # kill fail
+    else
+      return 1
+    fi
+
+  # process NOT found
+  else
+    msg_open DEFAULT "NO... "
+    return 0
+  fi
+}
+
+chk_at5() {
+  msg_open DEFAULT "[3/7] Kill ${PROC_NAME1} if running... "
+
+  if find_proc "${PROC_NAME1}"; then
+    stat GREEN "OK";
+
+  else
+    stat RED "FAIL"
+    msg_open RED "** ERROR ** "
+    msg_close RED "Failed to terminate ${PROC_NAME1}"
+    exit 1
+  fi
+}
+
+chk_ikpm() {
+  msg_open DEFAULT "[4/7] Kill ${PROC_NAME2} if running... "
+
+  if find_proc "${PROC_NAME2}"; then
+    stat GREEN "OK";
+
+  else
+    stat RED "FAIL"
+    msg_open RED "** ERROR ** "
+    msg_close RED "Failed to terminate ${PROC_NAME2}"
+    exit 1
+  fi
+}
+
+
+### 2
+# temporary root path
+tmp_path=""
+# ex.
+# a/b/AmpliTube 5 <-- tmp_path
+# a/b/AmpliTube 5/c
+# a/b/AmpliTube 5/c/d
+# ...
+# x/y/AmpliTube 5 <-- tmp_path
+# x/y/AmpliTube 5/z
+# x/y/AmpliTube 5/z/w
+
+# about "AmpliTube 5"
+do_list1() {
+  while IFS= read -r line; do
+    # if tmp_path is NOT null
+    if [[ -n "${tmp_path}" ]]; then
+
+      # current line include tmp_path
+      if [[ "${line}" == "${tmp_path}/"* ]]; then
+        continue
+
+      # current line is border (previous line was last child)
+      else
+        # flush
+        tmp_path=""
+      fi
+    fi
+    # from now and after, tmp_path IS null
+
+    # current line include "[Aa]mpli[Tt]ube\ ?5"
+    if [[ "$line" =~ [Aa]mpli[Tt]ube\ ?5 ]]; then
+      tmp_path="$line"
+      echo "/${tmp_path}" >> "${FILTERED}"
+
+    # very parent path, unrelated to AmpliTube 5
+    else
+      continue
+    fi
+  done < "${PKGFILES}"
+}
+
+# about "X-DRIVE", "X-SPACE", "X-TIME", "X-VIBE" (all case insensitive)
+do_list2() {
+  while IFS= read -r line; do
+    # if tmp_path is NOT null
+    if [[ -n "${tmp_path}" ]]; then
+
+      # current line include tmp_path
+      if [[ "${line}" == "${tmp_path}/"* ]]; then
+        continue
+
+      # current line is border (previous line was last child)
+      else
+        # flush
+        tmp_path=""
+      fi
+    fi
+
+    # current line include "X-(DRIVE|SPACE|TIME|VIBE)"(case insensitive)
+    if [[ "$line" =~ [Xx]-([Dd][Rr][Ii][Vv][Ee]|[Ss][Pp][Aa][Cc][Ee]|[Tt][Ii][Mm][Ee]|[Vv][Ii][Bb][Ee]) ]]; then
+      tmp_path="$line"
+      echo "/${tmp_path}" >> "${FILTERED}"
+
+    # very parent path, unrelated to AmpliTube 5
+    else
+      continue
+    fi
+  done < "${PKGFILES}"
+}
+
+do_list() {
+  msg_open DEFAULT "[5/7] Make list of files pkg wrote... "
+
+  # get full list of files which pkg installer write
+  pkgutil --files com.ikmultimedia.AmpliTube5 > "${PKGFILES}" 2>/dev/null || exit 1
+
+  # colon = null command
+  : > "${FILTERED}"
+
+  do_list1
+  do_list2
+
+  stat GREEN "OK"
+}
+
+do_remove() {
+  msg_open YELLOW "** Warning ** "
+  msg_close YELLOW "This is the point of no return."
+  msg_close RED "ALL FILES WILL BE PERMANENTLY DELETED."
+
+  if ! get_yn "Are you sure you want to proceed? Y(es)/ N(o) : "; then
+    msg_close DEFAULT "Deletion cancelled by user. Exiting safely..."
+    exit 0
+  fi
+
+  msg_open DEFAULT "[6/7] Remove files... "
+
+  # filtered.txt is empty
+  if [[ ! -s "${FILTERED}" ]]; then
+    stat RED "FAIL"
+    msg_open RED "** ERROR ** "
+    msg_close RED "File list is empty. Nothing to remove."
+    exit 1
+  fi
+
+  local err_cnt=0
+  while IFS= read -r line; do
+    # ignore empty line
+    [[ -z "$line" ]] && continue
+
+    # delete
+    if ! rm -rf "$line" 2>/dev/null; then
+      ((err_cnt++))
+    fi
+  done < "${FILTERED}"
+
+  if [[ $err_cnt -eq 0 ]]; then
+    stat GREEN "OK"
+
+  else
+    stat YELLOW "WARN"
+    msg_open YELLOW "** Warning ** "
+    msg_close YELLOW "Failed to remove $err_cnt items. Please check manually."
+  fi
+}
+
+do_cleanup() {
+  msg_open DEFAULT "[7/7] Final cleanup... "
+
+  pkgutil --forget "$PKG_NAME" > /dev/null 2>&1 || exit 1
+  rm -f "${PKGFILES}" "${FILTERED}" 2>/dev/null
+
+  stat GREEN "OK"
+}
+
+main() {
+  msg_close DEFAULT "[[ AmpliTube5 Uninstaller ]]"
 
   if ! get_yn "Are you sure you want to remove AmpliTube5? Y(es)/ N(o) : "; then
-    out_close "Exiting..."
+    msg_close DEFAULT "Exiting..."
     exit 0
   fi
 
   ### 1
-  chk_privilege
-  chk_package
-  chk_process
+  chk_priv
+  chk_pkg
+  chk_at5
+  chk_ikpm
 
   ### 2
-  do_make_list
-  do_format_list
-  do_remove_files
+  do_list
+  do_remove
+  do_cleanup
 
-  ### 3
-  cleanup
-
-  out_close "Uninstallation Complete!!"
+  msg_close GREEN "******* Uninstallation Complete!! *******"
 }
 
 main
